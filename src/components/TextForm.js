40a7af9f1fa7552b3988ccdfa6ae02b7625f91e0
nanashi <ny1113548@gmail.com>
2023-05-05 15:34:50 +0530

    added fetch email

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,26 +13,42 @@ export default function TextForm(props) {
 
         // setText("hoollahh")
     }
+    const fetchAllEmail = ()=>{
+      
+       // function for get email id
+        let email = " " 
+        email =  text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+        setText( email + "" )
+    }
     const handelOnChange = (event)=>{
         // setText("hoollahh")
         console.log("changed text")
         setText(event.target.value)
+
+
     }
-    const [text , setText] = useState("Enter Text Here")
+    const [text , setText] = useState("")
   return (
     <>
     <div className="container">
       <div className="mb-3">
         <h1 className="heading my-3">{props.heading}</h1>
         {/* <label htmlFor="myBox" className="form-label ">Example textarea</label> */}
-        <textarea className="form-control" value={text} onChange={handelOnChange} id="myBox" rows="18"></textarea>
+        <textarea className="form-control" value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={changeToUpCase}>Conver To Upper Case</button>
-      <button className="btn btn-primary  mx-5" onClick={changeToLoCase}>Conver To Lower Case</button>
+      <button className="btn btn-danger mx-1 my-1" onClick={changeToUpCase}>Conver To Upper Case</button>
+      <button className="btn btn-danger  mx-1 my-1" onClick={changeToLoCase}>Conver To Lower Case</button>
+      <button className="btn btn-danger  mx-1 my-1" onClick={fetchAllEmail}>Fetch All Emails</button>
     </div>
     <div className="container my-5">
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.trim().length } characters</p>
+        <p>{text.replace(/\n/g, " ").split(' ').filter(value => value != "").length} words</p>
+    </div>
+
+    <div className="container">
+      <h3>Preview</h3>
+      <p>{text}</p>
     </div>
     </>
   );
